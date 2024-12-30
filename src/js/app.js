625d3802003fddf3bebe2b3e59be4b212df1d94b
Maksim Chalyh <nazgulius@yandex.ru>
2024-12-31 00:09:18 +0300

    update: html -, app.js +

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,45 @@
+import img from '../img/goblin.png';
+
 document.addEventListener('DOMContentLoaded', () => {
-  const colAll = document.querySelectorAll('.col');
-  const goblin = document.querySelector('.goblin');
+  const table = document.createElement('table'); // создаётся таблицу 
+  table.className = "table";
+  document.body.append(table);
+
+  // создаём строки и столбцы таблицы 
+  for (let i = 0; i < 4; i++) {
+    const tr = document.createElement('tr');
+    tr.className = "row";
+    table.append(tr);
+
+    for (let i = 0; i < 4; i++) {
+      const td = document.createElement('td');
+      td.className = "col";
+      tr.append(td);
+    }
+  }
+
+  const colAll = document.querySelectorAll('.col'); // находит все col 
+
+  const imgGoblin = document.createElement('img'); // поменять на картинку img 
+  imgGoblin.className = "goblin";
+  imgGoblin.src = img;
+  imgGoblin.alt = "goblin";
+
+  colAll[0].appendChild(imgGoblin); // устанавливаем гоблина в первую ячейку
+
+  const goblin = document.querySelector('.goblin'); // находим гоблина 
+
 
   goblin.addEventListener('click', () => {
-    // добавляем гоблина в случайное место    
-    const randomIndex = Math.floor(Math.random() * colAll.length);
-    const position = colAll[randomIndex];
+    let currentIndex = Array.from(colAll).indexOf(imgGoblin.parentElement);
+    let randomIndex;
 
-    // перемещаем гоблина в новое место  
-    position.appendChild(goblin); 
+    // добавляем гоблина в случайное место
+    do {
+      randomIndex = Math.floor(Math.random() * colAll.length);
+    } while (randomIndex === currentIndex);
+
+    // перемещаем гоблина в новое место 
+    colAll[randomIndex].appendChild(imgGoblin);
   })
 })
